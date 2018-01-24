5fb6ace5a81bf1f07110635f8abf0653dbfb3841
suzan baert <suzan.baert@outlook.com>
2018-01-24 23:40:11 +0100

    latest kickstarter scrips

--- script_kickstarter.R
+++ script_kickstarter.R
@@ -2,43 +2,100 @@ library(dplyr)
 library(readr)
 library(tidyr)
 library(ggplot2)
+library(forcats)
 
 
 #intro on piping
 #use the pizza example
 #how does piping work? 
-#functions that take a dataframe
-#data is mentioned upfront
-
-#principle, even though this one is not really useful
-str(mtcars)
-mtcars %>%
-  str()
-
-#for functions with additional arguments
+#syntax of pipe operator
 
 
 
-#or more complicated
-ggplot(data = mtcars) +
-  geom_point(aes(x = mpg, y = wt))
-
-mtcars %>%
-  ggplot() +
-  geom_point(aes(x = mpg, y = wt))
-
-
+#DATA
 
 #data import
 #source data: kaggle (https://www.kaggle.com/kemical/kickstarter-projects/data)
-kickstarter_masterdata <- read_csv("data/kickstarter_data.csv")
+kickstarter <- read_csv("data/kickstarter_data_2018.csv")
+country_info <- read_csv("data/country_codes.csv")
 
 
 
-#SELECTING COLUMNS
 
-#original dataframe
-kickstarter_masterdata
+
+
+
+#CRAWL EXAMPLES
+
+#how many projects are successful versus other status's?
+kickstarter %>%
+  count(state) %>%
+  arrange(desc(n))
+
+
+kickstarter %>%
+  group_by(state) %>%
+  summarise(n = n(), percent = n/nrow(.)) %>%
+  arrange(desc(n))
+
+
+
+#which project received the most backers?
+kickstarter %>%
+  select(main_category, category, project = name, backers) %>%
+  arrange(desc(backers))
+
+
+
+#what different categories are there? 
+kickstarter %>%
+  group_by(main_category) %>%
+  count() %>%
+  arrange %>%
+  pull(main_category)
+
+
+#which category has the highest succes
+kickstarter %>%
+  count(main_category, state) %>%
+  spread(state, n) %>%
+  select(main_category, successful, failed) %>%
+  mutate(total = failed + successful,
+         prct_successful = successful/total) %>%
+  arrange(desc(prct_successful))
+
+
+#which category has the highest succes
+kickstarter %>%
+  count(main_category, state) %>%
+  spread(state, n) %>%
+  select(main_category, successful, failed) %>%
+  mutate(total = failed + successful,
+         prct_successful = successful/total) %>%
+  arrange(desc(total)) %>%
+  ggplot()+
+    geom_col(aes(x=fct_inorder(main_category), y=total, fill=prct_successful)) +
+    scale_fill_viridis_c(option="viridis", direction = -1, guide = guide_legend("Percent successful")) +
+    labs(title = "Number of kickstarters by category and percent successful",
+         x= "Category", y="Total amout of kickstarters")
+
+
+
+library(forcats)
+p + aes(x = fct_inorder(EffectNames))
+
+
+#filtering based on words
+
+kickstarter %>%
+  filter
+
+
+
+
+
+#which cat
+
 
 #selecting columns
 kickstarter_masterdata %>%
@@ -80,3 +137,7 @@ kickstarter_data %>%
   group_by(country) %>%
   summarise(n=n()) %>%
   arrange(desc(n))
+
+
+
+
